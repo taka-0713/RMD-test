ce20a4a97defc4564dda6a91c75f4518ae71809f
taka-0713 <ottake07@gmail.com>
2021-04-06 14:56:07 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function() {
+  // メニュー表示イベント
+  $("#menu").click(function(){
+    $(".menu-box").fadeIn();
+  });
+  // スクロールイベント（未完成）
+  // $(top).scroll(function(){
+  //   $(".top").fadeOut(1500);
+  // });
+  
+});
